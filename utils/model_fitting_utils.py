b023c0eae12746eb1757f580e9013d0fe705c417
NayanY176 <yadav.nayan2006@gmail.com>
2025-07-18 11:06:15 -0400

    Update model_fitting_utils.py

--- utils/model_fitting_utils.py
+++ utils/model_fitting_utils.py
@@ -808,7 +808,7 @@ initial_params = {
         'n_jobs': -1
     },
     'random_forest': { # New: Default parameters for Random Forest Regressor
-        'n_estimators': 20,
+        'n_estimators': 5,
         'max_depth': None,
         'criterion': 'squared_error',
         'n_jobs': -1,
