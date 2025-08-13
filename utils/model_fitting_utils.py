0a50d491957f1cee871c8dcc6e3f9be435139165
NayanY176 <yadav.nayan2006@gmail.com>
2025-08-13 10:09:38 -0400

    Update model_fitting_utils.py

--- utils/model_fitting_utils.py
+++ utils/model_fitting_utils.py
@@ -80,8 +80,8 @@ initial_params = {
     'lightgbm': {
         'objective': 'regression_l1',
         'metric': 'rmse',
-        'n_estimators': 150,
-        'learning_rate': 0.05,
+        'n_estimators': 1500,
+        'learning_rate': 0.03,
         'num_leaves': 31,
         'max_depth': -1,
         'min_child_samples': 20,
